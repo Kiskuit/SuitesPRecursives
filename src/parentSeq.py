2eaf861fabf7730ed54e88761fd841f0e19538ac
Mathis CARISTAN <caristan@asclepios.calsci.lip6.fr>
2017-06-26 17:04:37 +0200

    Coercion 90%

--- src/parentSeq.py
+++ src/parentSeq.py
@@ -13,7 +13,7 @@ from __future__ import print_function
 from sage.rings.ring import Ring
 from sage.structure.unique_representation import UniqueRepresentation
 from sage.all import *
-from ore_algebra import OreAlgebra
+from ore_algebra import OreAlgebra, guess
 from seq import PRecSequence
 
 # TODO change class it inherits from ?
@@ -44,11 +44,41 @@ class ParentSeqRec (Ring, UniqueRepresentation):
         return ret
 
     def _element_constructor_(self, *args, **kwargs):
-        # TODO why 1?
-        print (args, len(args))
         if len(args) != 1:
-            pass
-            #return self.element_class (self, *args, **kwargs)
+            return self.element_class (self, *args, **kwargs)
+        x = args[0]
+        try :
+            P = x.parent()
+        except AttributeError :
+            # TODO what if attributerror?
+            return self.element_class (self, x, **kwargs)
+
+        # x is in base_ring()
+        # TODO take into account cases where there are roots to the pol
+        # TODO lower boundary guessing --> `8` hard coded but ugly
+        if x in self.base_ring() :
+            gen = self.base_ring().gen()
+            condInit = x.subs({gen:0})
+            annihil = -guess ([x.subs({gen:i}) for i in range(8)], self._ore_algebra)
+            return self.element_class (self, condInit, annihil)
+
+        # x is a constant of val_ring
+        #   return with annihilator Sn - 1 (where Sn is replaced by actual generator)
+        if x in self._val_ring :
+            return self.element_class (self, {0:x}, self._ore_algebra.gen() - 1)
+
+        # Default case
+        return self.element_class (self, x, **kwargs)
+
+    def _coerce_map_from_ (self, S):
+        if self.ore_algebra().has_coerce_map_from (S):
+            return True
+        if self._val_ring.has_coerce_map_from (S):
+            return True
+        
+        # Default case 
+        return False
+
 
 
 if __name__ == "__main__":
