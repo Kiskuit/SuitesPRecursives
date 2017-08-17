fec61c1df8b78341b6c60c28c9c491aaa9fffd71
Mathis CARISTAN <mathis.caristan@etu.upmc.fr>
2017-08-17 15:21:35 +0200

    Update script OEIS

--- src/database.py
+++ src/database.py
@@ -39,25 +39,30 @@ def getSequenceFromPage(url):
     table = map(lambda x: int(re.sub(pattern,'',x.string)), table)
     seq = {}
     k,v = table[0::2], table[1::2]
-    return dict(zip(k,v))
+    if k[-1] != k[0] + len(k) -1:
+        # Case where the indices are not consecutives
+        return None
+    return (k[0],v)
 
-def browsePages():
-    maxSeq = 200
+def browsePages(start, stop):
     dbfile = '../db/properties.csv'
     # Progress vars
     i_progress = 1
-    progress = int(maxSeq)/int(10)
+    progress = int(stop-start)/int(10)
     # Empty file
     open(dbfile,'w').close()
     # Browse all pages
-    for indexSeq in range(1,maxSeq):
+    for indexSeq in range(start, stop):
         url = "https://oeis.org/A%06d/list" % indexSeq
         if indexSeq == i_progress*progress:
             print ("progress : %d0%%" % i_progress)
             i_progress += 1
         seq = getSequenceFromPage(url)
-        
-        annihilator = guess(seq)
+        if seq is None:
+            continue
+        else:
+            start,seq = seq
+        annihilator = guess(seq, start)
         if annihilator is not None :
             order = annihilator.order()
             cond = seq[:order]
@@ -65,7 +70,7 @@ def browsePages():
                 fd.write("A%06d,%s,%s\n" % (indexSeq, annihilator, cond))
 
 
-def guess(seq):
+def guess(seq, start):
     Seqs = ppseq.ParentPRecursiveSequences(ZZ['n'])
     
     for i in range(10,30):
@@ -73,16 +78,32 @@ def guess(seq):
         if i > len(seq):
             return None # TODO handle it better?
         try:
-            annihilator = ore_algebra.guess(seq[:i], alg)
+            annihilator = ore_algebra.guess(seq[:i], alg, start=start)
         except ValueError:
             continue
         # TODO check if annihilator is good or not
-        seq_guessed = annihilator.to_list(seq[:annihilator.order()], len(seq))
+        seq_guessed = annihilator.to_list(seq[:annihilator.order()], len(seq), start=start)
         if seq_guessed == seq :
             return annihilator
     return None
 
 if __name__ == "__main__":
-    print (getSequenceFromPage("https://oeis.org/A005206/list"))
-    # browsePages()
+    #print (getSequenceFromPage("https://oeis.org/A005206/list"))
+    from sys import argv
+    if len(argv) == 1:
+        start = 1
+        stop = maxSeq
+    elif len(argv) == 2:
+        start = 1
+        # TODO
+        stop = 200
+        ###   stop = int(argv[1])
+    ###   elif len(argv) > 2:
+    ###       start = int(argv[1])
+    ###       stop = int(argv[2])
+    else:
+        ValueError("Please call this script ./database.py `start` `stop` or ./database `stop`")
+    if stop < start or start < 0:
+        raise ValueError("Please provide positive integers such that start < stop")
+    browsePages(start, stop)
     
