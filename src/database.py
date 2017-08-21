9514867b6b96b773143b894ce7cd2ab0004406e1
Mathis Caristan <m.caristan@hotmail.fr>
2017-08-21 19:08:56 +0200

    Read seqs/offsets from files instead of webpages

--- src/database.py
+++ src/database.py
@@ -10,8 +10,8 @@
 from __future__ import print_function
 
 # Python imports
-import urllib2
-from bs4 import BeautifulSoup
+###   import urllib2
+###   from bs4 import BeautifulSoup
 import re
 import csv
 import parent_p_recursive_sequences as ppseq
@@ -24,8 +24,31 @@ import ore_algebra
 magicNumber = 4
 maxSeq = 289480
 
-def getSequenceFromPage(url):
+def getSequenceFromTxt(seq_filename, offset_filename):
+    dbfile = "test.txt"
+    seq_fd = open (seq_filename, 'r')
+    offset_fd = open(offset_filename, 'r')
+    # TODO iterate though the min of both files?
+    for line in seq_fd:
+        seq = parseSeq(seq_fd.readline())
+        start = parseOffset(offset_rd.readline())
+        annihilator = guess(seq, start)
+        if annihilator is not None :
+            order = annihilator.order()
+            cond = seq[:order]
+            with open(dbfile, 'a') as fd:
+                fd.write("A%06d,%s,%s\n" % (indexSeq, annihilator, cond))
 
+def parseOffset(line):
+    line = line[11:]
+    line = line.split(",")[0]
+    return int(line)
+
+def parseSeq(line):
+    # TODO
+    return [0,1,1,2,3,5,8,13,21,34,55]
+
+def getSequenceFromPage(url):
     # Open url and get content
     sloanePage = urllib2.urlopen(url)
     sloaneSoup = BeautifulSoup(sloanePage, "lxml")
@@ -87,23 +110,26 @@ def guess(seq, start):
             return annihilator
     return None
 
-if __name__ == "__main__":
-    #print (getSequenceFromPage("https://oeis.org/A005206/list"))
-    from sys import argv
-    if len(argv) == 1:
-        start = 1
-        stop = maxSeq
-    elif len(argv) == 2:
-        start = 1
-        # TODO
-        stop = 200
-        ###   stop = int(argv[1])
-    ###   elif len(argv) > 2:
-    ###       start = int(argv[1])
-    ###       stop = int(argv[2])
-    else:
-        ValueError("Please call this script ./database.py `start` `stop` or ./database `stop`")
-    if stop < start or start < 0:
-        raise ValueError("Please provide positive integers such that start < stop")
-    browsePages(start, stop)
+###   if __name__ == "__main__":
+###       #print (getSequenceFromPage("https://oeis.org/A005206/list"))
+###       from sys import argv
+###       if len(argv) == 1:
+###           start = 1
+###           stop = maxSeq
+###       elif len(argv) == 2:
+###           start = 1
+###           # TODO
+###           stop = 200
+###           ###   stop = int(argv[1])
+###       ###   elif len(argv) > 2:
+###       ###       start = int(argv[1])
+###       ###       stop = int(argv[2])
+###       else:
+###           ValueError("Please call this script ./database.py `start` `stop` or ./database `stop`")
+###       if stop < start or start < 0:
+###           raise ValueError("Please provide positive integers such that start < stop")
+###       browsePages(start, stop)
     
+if __name__ == "__main__":
+    getSequenceFromTxt("seq.txt", "offset.txt")
+
